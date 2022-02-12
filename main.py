e70de34e7ae4c3e9752578381ddf6a0bc453f935
var1ables <67993562+var1ables@users.noreply.github.com>
2022-02-11 21:17:36 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas
 
-data = pandas.read_csv('nato_phonetic_alphabet.csv')
-nato = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user = input('What do you want translated to nato phonetic?').upper()
-answer = [nato[letter] for letter in user]
-print(answer)
+def generate():
+    data = pandas.read_csv('nato_phonetic_alphabet.csv')
+    nato = {row.letter: row.code for (index, row) in data.iterrows()}
+
+    user = input('What do you want translated to nato phonetic?').upper()
+    try:
+        answer = [nato[letter] for letter in user]
+    except KeyError:
+        print('Only letters please')
+        generate()
+    else:
+        print(answer)
+
+generate()
